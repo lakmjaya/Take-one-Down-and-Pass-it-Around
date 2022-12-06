b07c8747703a66d0ba1b5a06d6a4a96c0e59313f
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-12-06 00:28:37 -0500

    final

--- server.js
+++ server.js
@@ -11,14 +11,21 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:number_of_bottles', function(req, res) {
-    res.send(req.params.number_of_bottles + " The number of bottles of beer on the wall <a href=/(req.params.number_of_bottles-1)>take one down, pass it around</a> " );
+    let bottles = req.params.number_of_bottles;
+    if(bottles <=0){
+        res.send(` ${bottles} Bottles of beer left <br> <a href='/'><h1>a link to start over </h1></a>`);
+    }else{
+        res.send(`${bottles} The number of bottles of beer on the wall <a href='/${bottles -1}'>take one down, pass it around</a> `);
+    }
+    
 });
-app.get('/:0', function(req, res) {
-    res.send("99 Bottles of beer on the wall <br> <a href=/><h1>a link to start over </h1></a>");
-});
+
+
 
 
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
  console.log('Listening on port 3000');
 });
+
+
